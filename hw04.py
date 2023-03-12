284d5fc21cfde88d6bc86d3039d16ad737c8106a
mac <mac@macs-MacBook-Air.local>
2023-03-12 15:37:20 -0700

    First adding of of lab

--- hw04.py
+++ hw04.py
@@ -64,13 +64,14 @@ def end(s):
 def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
-    "*** YOUR CODE HERE ***"
+    list = ['planet', mass]
+    return list
 
 
 def mass(w):
     """Select the mass of a planet."""
     assert is_planet(w), 'must call mass on a planet'
-    "*** YOUR CODE HERE ***"
+    return w[1]
 
 
 def is_planet(w):
@@ -126,7 +127,12 @@ def balanced(m):
     >>> check(HW_SOURCE_FILE, 'balanced', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return True
+    return length(left(m)) * total_weight(end(left(m))) == length(right(m)) * total_weight(end(right(m))) and balanced(end(left(m))) and balanced(end(right(m)))
+
+    
+    """return length(left(m)) * total_weight(end(left(m))) == length(right(m)) * total_weight(end(right(m)))"""
 
 
 def totals_tree(m):
@@ -158,7 +164,10 @@ def totals_tree(m):
     >>> check(HW_SOURCE_FILE, 'totals_tree', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    if is_planet:
+        return tree(m)
+    return tree()
+    
 
 
 def replace_loki_at_leaf(t, lokis_replacement):
@@ -190,9 +199,13 @@ def replace_loki_at_leaf(t, lokis_replacement):
     >>> laerad == yggdrasil # Make sure original tree is unmodified
     True
     """
-    "*** YOUR CODE HERE ***"
-
-
+    
+    
+    if(is_leaf(t) and label(t) == 'loki'):
+        return tree(lokis_replacement)
+    return tree(label(t), [replace_loki_at_leaf(b, lokis_replacement) for b in branches(t)])
+    
+    
 def has_path(t, word):
     """Return whether there is a path in a tree where the entries along the path
     spell out a particular word.
@@ -224,7 +237,14 @@ def has_path(t, word):
     False
     """
     assert len(word) > 0, 'no path for empty word.'
-    "*** YOUR CODE HERE ***"
+    if(len(word) == 1):
+        return label(t) == word
+    elif label(t) != word[0]:
+        return False
+    for branch in branches(t):
+        if has_path(branch, word[1:]):
+            return True
+    return False
 
 
 def str_interval(x):
@@ -248,12 +268,12 @@ def interval(a, b):
 
 def lower_bound(x):
     """Return the lower bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[0]
 
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[1]
 
 
 def str_interval(x):
@@ -272,24 +292,25 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
-    "*** YOUR CODE HERE ***"
+    minusY = interval(-(upper_bound(y)), -(lower_bound(y)))
+    return add_interval(x, minusY)
 
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
-    "*** YOUR CODE HERE ***"
+    assert not (lower_bound(y) <= 0 and 0 <= upper_bound(y))
     reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -314,8 +335,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 2)  # Replace this line!
+    r2 = interval(3, 4)  # Replace this line!
     return r1, r2
 
 
